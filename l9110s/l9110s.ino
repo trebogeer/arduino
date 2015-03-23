bc5eed9eda69711001b2421f96f7298314c26b6f
Dmitry Vasilyev <trebogeer@gmail.com>
2015-03-22 18:22:32 -0700

    obstacle avoidance

--- l9110s/l9110s.ino
+++ l9110s/l9110s.ino
@@ -18,7 +18,7 @@
 // the actual values for "fast" and "slow" depend on the motor
 #define PWM_SLOW 150  // arbitrary slow speed PWM duty cycle
 #define PWM_FAST 250 // arbitrary fast speed PWM duty cycle
-#define DIR_DELAY 1000 // brief delay for abrupt motor changes
+#define DIR_DELAY 200//1000 // brief delay for abrupt motor changes
 #define TURN_DELAY 3000
 
 #define TRIGGER_PIN  13  // Arduino pin tied to trigger pin on the ultrasonic sensor.
@@ -40,10 +40,10 @@ void setup()
   pinMode( MOTOR_B_PWM, OUTPUT );
   digitalWrite( MOTOR_B_DIR, LOW );
   digitalWrite( MOTOR_B_PWM, LOW );
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  pinMode(led, OUTPUT);
-  pinMode(led2, OUTPUT);
+  pinMode(TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(LED0, OUTPUT);
+  pinMode(LED1, OUTPUT);
 }
 
 int state = 0;
@@ -53,7 +53,13 @@ void loop()
   byte c = ultrasonicPing();
 
   do {
-    delay( DIR_DELAY );
+    /*if(c == 7 || c == 8) {
+      delay( TURN_DELAY );
+    } else {
+      //delay( DIR_DELAY );
+      delay(  PING_DELAY );
+    }*/
+    delay(60);
     c = ultrasonicPing();
   } 
   while (state == c);
@@ -111,7 +117,7 @@ void loop()
     break;      
 
   case 5: // Fast reverse
-    Serial.println( "Fast forward..." );
+    Serial.println( "Fast reverse..." );
     // always stop motors briefly before abrupt changes
     stopMotors();
     delay( DIR_DELAY );
@@ -138,9 +144,9 @@ void loop()
     delay( DIR_DELAY );
     // set the motor speed and direction
     digitalWrite( MOTOR_A_DIR, HIGH ); // direction = forward
-    analogWrite( MOTOR_A_PWM, 255-PWM_SLOW ); // PWM speed = slow
-    digitalWrite( MOTOR_B_DIR, LOW ); // direction = forward
-    analogWrite( MOTOR_B_PWM, PWM_SLOW ); // PWM speed = slow
+    analogWrite( MOTOR_A_PWM, 255-PWM_FAST ); // PWM speed = fast
+    digitalWrite( MOTOR_B_DIR, LOW ); // direction = reverse
+    analogWrite( MOTOR_B_PWM, PWM_FAST ); // PWM speed = fast
     break;
 
   case 8: // Turn right      
@@ -150,10 +156,10 @@ void loop()
 
     delay( DIR_DELAY );
     // set the motor speed and direction
-    digitalWrite( MOTOR_A_DIR, LOW ); // direction = forward
-    analogWrite( MOTOR_A_PWM, PWM_SLOW ); // PWM speed = slow
+    digitalWrite( MOTOR_A_DIR, LOW ); // direction = reverse
+    analogWrite( MOTOR_A_PWM, PWM_FAST ); // PWM speed = fast
     digitalWrite( MOTOR_B_DIR, HIGH ); // direction = forward
-    analogWrite( MOTOR_B_PWM, 255-PWM_SLOW ); // PWM speed = slow
+    analogWrite( MOTOR_B_PWM, 255-PWM_FAST ); // PWM speed = fast
     break;
 
   default:
@@ -178,15 +184,18 @@ int ultrasonicPing() {
   delayMicroseconds(10);
   digitalWrite(TRIGGER_PIN, LOW);
   duration = pulseIn(ECHO_PIN, HIGH);
-  distance = (duration/2) / 29.1;
+  distance = duration/ 58.2;
   Serial.print(distance);
   Serial.println(" cm");
-  if (distance < 15 && distance > 0) { 
+  if (distance < 15 && distance >= 0) { 
+    digitalWrite(LED0,HIGH);
+    digitalWrite(LED1,LOW);
+    return 5;
+  }else if(distance >= 15 && distance <= 30) {
     digitalWrite(LED0,HIGH);
     digitalWrite(LED1,LOW);
     return 7;
-  }
-  else {
+  } else {
     digitalWrite(LED0,LOW);
     digitalWrite(LED1,HIGH);
     return 1;
